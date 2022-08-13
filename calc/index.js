840ac2531dd2395320e4009d36397c97262c8d1f
exp1x1 <gorvkrtianliya@gmail.com>
2022-08-13 14:47:36 +0530

    a

---
+++ calc/index.js
@@ -0,0 +1,9 @@
+const allButtons = document.querySelectorAll('.button');
+
+allButtons.forEach( (e) => {
+    e.addEventListener('click', () => {console.log()})
+} );
+
+function buttonAction(){
+
+}
